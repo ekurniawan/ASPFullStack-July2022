c1305caf680a9bc883bf69f42908c07de433664c
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-08-02 16:31:08 +0700

    menambahkan register

--- MyASPProject/ViewModels/RegisterViewModel.cs
+++ MyASPProject/ViewModels/RegisterViewModel.cs
@@ -1,9 +1,21 @@
-﻿namespace MyASPProject.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyASPProject.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirmation Password")]
+        [Compare("Password",ErrorMessage ="Password dan Confirm Password tidak sama")]
+        public string ConfirmPassword { get; set; }
 
     }
 }
