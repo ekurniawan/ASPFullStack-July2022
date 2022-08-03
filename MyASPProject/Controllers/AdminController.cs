8f800e313666b2590663b9a3f7facf554a36c828
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-08-03 14:47:45 +0700

    menambahkan tampil role

---
+++ MyASPProject/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyASPProject.ViewModels;
+
+namespace MyASPProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole myRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                var result = await _roleManager.CreateAsync(myRole);
+                if(result.Succeeded)
+                {
+                    ViewData["pesan"] = 
+                        $"<div class='alert alert-success alert-dismissible fade show'><button type='button' class='btn-close' data-bs-dismiss='alert'></button> Berhasil menambahkan data role {model.RoleName}</div>"; 
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+    }
+}
